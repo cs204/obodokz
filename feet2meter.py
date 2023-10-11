ed1d118a5501e33366988ea93523a5a6f2ff6dab
test <test>
2023-10-11 18:44:25 +0700

    test

---
+++ feet2meter.py
@@ -0,0 +1,13 @@
+def feet2meter(v):
+    feet = float(v.replace('ft', ''))
+    meters = feet * 0.3048
+
+    return meters
+
+
+def main():
+    v = feet2meter(input("Сколько футов:"))
+    print(f"Это будет {v:.2f} метров.")
+
+
+main()
